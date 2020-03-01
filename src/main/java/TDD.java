f2db341d617b949f46bd31a6aa974955d43e5417
asiakostrzewa <joanna.o.kostrzewa@tlen.pl>
2020-03-01 15:07:20 +0100

    TDD

---
+++ src/main/java/TDD.java
@@ -0,0 +1,14 @@
+public class TDD {
+
+    public double powValue(double val, int pow) { //int bo potęga musi być typu całkowitego
+        return Math.pow(val, pow);
+    }
+
+    public double divValue(double val1, double val2) {
+            return val1 / val2;
+    }
+
+    public double sqrtValue(double val) {
+        return Math.pow(val, 2);
+    }
+}
---
+++ src/test/java/TDDTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TDDTest {
+
+    private TDD tdd;
+
+    @BeforeEach
+    void setUp() {
+        tdd = new TDD();
+    }
+
+    @Test
+    void powValue() {
+        int expected = 16;
+        double actual = tdd.powValue(4, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void divValues() {
+        assertThat(tdd.divValue(16, 4))
+                .isGreaterThan(0)
+                .isEqualTo(4)
+                .isNotNull();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "4:16",
+            "0:5",
+            "-10:100"
+    }, delimiter = ':')
+    void sqrtValue(double val, double expected) {
+        assertEquals(
+                expected,
+                tdd.sqrtValue(val)
+        );
+    }
+}
